6973e8e9c1f41cb860fe2b1b4f8b4e0d5c919b57
Peter Lunneberg <plunn.dev@gmail.com>
2014-01-11 21:53:17 -0500

    Tweaked the CharSheet constructor. Worked on layouts.
    Updated the CharSheet constructor such that it is not explicitly coupled
    with the Android SDK.
    
    Started piecing together a more finalized structure for the layouts.
    Main view will be character list. Each item in the list will represent a
    single character sheet.
    
    In portrait orientation, a character sheet list item will display Name
    and Soul Level (Starting Class as well, if there is room). The Add New
    Character Sheet button will be at the bottom of the screen, for
    convenience, as well as in the menu.
    
    In landscape orientation Name, Soul Level, and Attribute Scores. The Add
    New Character Sheet button will only be in the menu as this is intended
    more as a quick comparison between characters.

--- DarkSouls2CharSheet/src/org/nu/ds2cs/MainActivity.java
+++ DarkSouls2CharSheet/src/org/nu/ds2cs/MainActivity.java
@@ -1,6 +1,7 @@
 package org.nu.ds2cs;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import org.nu.ds2cs.data.CharSheet;
 import org.nu.ds2cs.data.StartingClass;
@@ -12,8 +13,45 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+		setContentView(R.layout.character_list);
+		
+		/*
+		CharSheet test = testMethod();
+		List<String> testList = new ArrayList();
+		testList.add(test.getName());
+		testList.add(String.valueOf(test.getSoulLevel()));
+		
+		List<String>[] testArr = new List[1];
+		testArr[0] = testList;
+		
+		ListAdapter listAdapter = new ArrayAdapter<List<String>>(this, R.layout.character_list_item, testArr);
+		setContentView(R.layout.character_list);
+		ListView listView = (ListView)findViewById(R.id.characterList);
+		listView.setAdapter(listAdapter);
+		*/
+		
+		/*
         setContentView(R.layout.sheet_list_item);
 		
-		CharSheet test = new CharSheet(getApplicationContext(), "Tester McTesterson", StartingClass.DualSwordsman);
+		Resources res = getResources();
+		startingLevel = res.getInteger(res.getIdentifier(startingClass.key + "_level", "integer", context.getPackageName()));
+		int[] startingAttributes = res.getIntArray(res.getIdentifier(startingClass.key + "_attributes", "array", context.getPackageName()));
+		CharSheet test = new CharSheet("Tester McTesterson", StartingClass.DualSwordsman, startingLevel, startingAttributes);
+		
+		TextView name = (TextView)findViewById(R.id.charName);
+		TextView level = (TextView)findViewById(R.id.soulLevel);
+		
+		name.setText(test.getName());
+		level.setText(String.valueOf(test.getSoulLevel()));
+		*/
     }
+	
+	public CharSheet testMethod()
+	{
+		Resources res = getResources();
+		int startingLevel = res.getInteger(res.getIdentifier(StartingClass.DualSwordsman.key + "_level", "integer", getPackageName()));
+		int[] startingAttributes = res.getIntArray(res.getIdentifier(StartingClass.DualSwordsman.key + "_attributes", "array", getPackageName()));
+		CharSheet test = new CharSheet("Tester McTesterson", StartingClass.DualSwordsman, startingLevel, startingAttributes);
+		return test;
+	}
 }
--- DarkSouls2CharSheet/src/org/nu/ds2cs/data/CharSheet.java
+++ DarkSouls2CharSheet/src/org/nu/ds2cs/data/CharSheet.java
@@ -1,8 +1,5 @@
 package org.nu.ds2cs.data;
 
-import android.content.Context;
-import android.content.res.Resources;
-
 /**
  * 
  * @author peter
@@ -11,31 +8,32 @@ public class CharSheet
 {
 	private String name;
 	private StartingClass startingClass;
+	private Integer startingLevel;
 	private Attributes startingAttributes;
 	private Attributes attributes;
 	
-	public CharSheet(Context context, String name, StartingClass startingClass)
+	public CharSheet(String name, StartingClass startingClass, int startingLevel, int[] startingAttributes)
 	{
 		this.name = name;
 		this.startingClass = startingClass;
-		
-		Resources res = context.getResources();
-		int[] startingAttr = res.getIntArray(res.getIdentifier(startingClass.key, "array", context.getPackageName()));
-		startingAttributes = new Attributes(startingAttr);
-		attributes = new Attributes(startingAttr);
+		this.startingLevel = startingLevel;
+		this.startingAttributes = new Attributes(startingAttributes);
+		this.attributes = new Attributes(startingAttributes);
 	}
 	
-	public int getStartingLevel() { return startingAttributes.getCurrentTotal(); }
-	public int getSoulLevel() { return attributes.getCurrentTotal(); }
+	public int getSoulLevel() { return startingLevel + (attributes.getCurrentTotal() - startingAttributes.getCurrentTotal()); }
 	public int getMaxLevel() { return Attributes.AttributeTotalMax - getStartingLevel(); }
 	
 	public Attributes getAttributes() { return attributes; }
 	public String getName() { return name; }
 	public Attributes getStartingAttributes() { return startingAttributes; }
+	public Integer getStartingLevel() { return startingLevel; }
 	public StartingClass getStartingClass() { return startingClass; }
 	
 	public void setAttributes(Attributes attributes) { this.attributes = attributes; }
 	public void setName(String name) { this.name = name; }
-	public void setStartingAttributes(Attributes startingAttributes) { this.startingAttributes = startingAttributes; }
 	public void setStartingClass(StartingClass startingClass) { this.startingClass = startingClass; }
+	
+	protected void setStartingAttributes(Attributes startingAttributes) { this.startingAttributes = startingAttributes; }
+	protected void setStartingLevel(Integer startingLevel) { this.startingLevel = startingLevel; }
 }
