a91d0d69f7d2fdce5fbe9576c7a05d2e304376d9
Peter Lunneberg <plunn.dev@gmail.com>
2014-01-21 21:38:28 -0500

    Updated character list and character sheet layout.
    Character List layout is at "mock up" stage.
    Character Sheet layout is only beginning to take form.

--- DarkSouls2CharSheet/src/org/nu/ds2cs/MainActivity.java
+++ DarkSouls2CharSheet/src/org/nu/ds2cs/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.character_list);
+		setContentView(R.layout.character_sheet);
 		
 		/*
 		CharSheet test = testMethod();
--- DarkSouls2CharSheet/src/org/nu/ds2cs/data/CharSheet.java
+++ DarkSouls2CharSheet/src/org/nu/ds2cs/data/CharSheet.java
@@ -11,6 +11,7 @@ public class CharSheet
 	private Integer startingLevel;
 	private Attributes startingAttributes;
 	private Attributes attributes;
+	private Integer humanity;
 	
 	public CharSheet(String name, StartingClass startingClass, int startingLevel, int[] startingAttributes)
 	{
@@ -19,6 +20,7 @@ public class CharSheet
 		this.startingLevel = startingLevel;
 		this.startingAttributes = new Attributes(startingAttributes);
 		this.attributes = new Attributes(startingAttributes);
+		this.humanity = 0;
 	}
 	
 	public int getSoulLevel() { return startingLevel + (attributes.getCurrentTotal() - startingAttributes.getCurrentTotal()); }
@@ -29,10 +31,12 @@ public class CharSheet
 	public Attributes getStartingAttributes() { return startingAttributes; }
 	public Integer getStartingLevel() { return startingLevel; }
 	public StartingClass getStartingClass() { return startingClass; }
+	public Integer getHumanity() { return humanity; }
 	
 	public void setAttributes(Attributes attributes) { this.attributes = attributes; }
 	public void setName(String name) { this.name = name; }
 	public void setStartingClass(StartingClass startingClass) { this.startingClass = startingClass; }
+	public void setHumanity(Integer humanity) { this.humanity = humanity; }
 	
 	protected void setStartingAttributes(Attributes startingAttributes) { this.startingAttributes = startingAttributes; }
 	protected void setStartingLevel(Integer startingLevel) { this.startingLevel = startingLevel; }
